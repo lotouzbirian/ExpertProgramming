01bfa3a160998f02ab1a26ba13875d1d101a838b
Leandro Otouzbirian <leandro.otouzbirian@sabf.org.ar>
2016-10-12 15:31:12 -0300

    Interfaz para completar la lista

---
+++ list.c
@@ -0,0 +1,30 @@
+
+
+
+struct ListCDT{
+	struct ListCDT *sig;
+	int elem;
+};
+
+typedef struct node *ListADT;
+
+ListADT
+new(void);
+
+void
+free(ListADT);
+
+int
+deleteElem(ListADT, int index);
+
+int
+insertElem(ListADT); /* Se insertan atrás */
+
+int
+getElem(ListADT, int index);
+
+int
+countElem(ListADT);
+
+int
+maxElem(ListADT);
